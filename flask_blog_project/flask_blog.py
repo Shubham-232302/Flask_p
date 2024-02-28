5bc86c4341c9f7b0b325c37c48d030a929754b47
Shubham <sdeodhar47@gmail.com>
2024-02-29 00:13:52 +0530

    added posts and html,css

--- flask_blog_project/flask_blog.py
+++ flask_blog_project/flask_blog.py
@@ -1,14 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
+posts = [
+    {
+    'author': 'Shubham Deodhar',
+    'title': 'My First Blog',
+    'content': 'This is the content',
+    'date':'28-feb-2024'
+        },
+    {
+        'author': 'new user',
+    'title': 'new Blog',
+    'content': 'This is the new content',
+    'date':'28-feb-1900'
+    }
+]
+
 @app.route("/")
-def hello():
-    return "<h1>Hello World</h1>"
+def home():
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about_page():
-    return("<h1>About Page<h1>")
+    return render_template("about.html", title="about")
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+    
+    #Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
