17b8e5fbc525c29634f50f5ae90d5bbae0543fd8
Shubham <sdeodhar47@gmail.com>
2024-03-02 03:24:26 +0530

    login and register pages

--- flask_blog_project/flask_blog.py
+++ flask_blog_project/flask_blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, flash
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -31,12 +31,20 @@ def about_page():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}')
+        flash(f'Account created for {form.username.data}','success')
+        return redirect(url_for('home'))
     return render_template('register.html', title = 'Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@gmail.com' and form.password.data == "12345":
+            flash(f'You have been logged in','success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful please check username and password!!', 'danger')
+                
     return render_template('login.html', title = 'Login', form=form)
 
 
