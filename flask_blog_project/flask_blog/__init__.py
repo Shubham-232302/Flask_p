0fcc68a52456eb6ba382faf3106917e20436f7b9
Shubham <sdeodhar47@gmail.com>
2024-03-03 03:56:35 +0530

    Authorization

--- flask_blog_project/flask_blog/__init__.py
+++ flask_blog_project/flask_blog/__init__.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-from datetime import datetime
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '0a0e56d1dba88a11623a53a2dc0472db'
@@ -9,5 +9,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.app_context().push()
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flask_blog import routes
--- flask_blog_project/flask_blog/forms.py
+++ flask_blog_project/flask_blog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email,EqualTo
+from wtforms.validators import DataRequired, Length, Email,EqualTo, ValidationError
+from flask_blog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=30)])
@@ -8,6 +9,19 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up!!!')
+    
+    def validate_username(self, username):
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError(f"username '{username.data}' already exists, Please choose another one")
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(email = email.data).first()
+        if user:
+            raise ValidationError(f"email '{email.data}' already exists, Please choose another one")
+        
+        
+        
     
     
 class LoginForm(FlaskForm):
